78b1a21ba6f2ba019391c0676edda1a8c094d079
Rex.G <rex1109@hotmail.com>
2018-07-25 09:27:01 +0800

    implemented to stm32 , preparing do the stepping of motor

---
+++ Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.d
@@ -0,0 +1,71 @@
+Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.o: \
+ ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ C:/REX/StepperMotor/Inc/stm32f1xx_hal_conf.h \
+ C:/REX/StepperMotor/Inc/main.h \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/REX/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/REX/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/REX/StepperMotor/Drivers/CMSIS/Include/core_cm3.h \
+ C:/REX/StepperMotor/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/REX/StepperMotor/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/REX/StepperMotor/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/REX/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+C:/REX/StepperMotor/Inc/stm32f1xx_hal_conf.h:
+
+C:/REX/StepperMotor/Inc/main.h:
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+C:/REX/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+C:/REX/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+C:/REX/StepperMotor/Drivers/CMSIS/Include/core_cm3.h:
+
+C:/REX/StepperMotor/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/REX/StepperMotor/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/REX/StepperMotor/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/REX/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+
+C:/REX/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
---
+++ Debug/Src/InfoVerifier.d
@@ -0,0 +1,16 @@
+Src/InfoVerifier.o: ../Src/InfoVerifier.c \
+ C:/REX/StepperMotor/Inc/InfoVerifier.h \
+ C:/REX/StepperMotor/Inc/MotorController.h \
+ C:/REX/StepperMotor/vendor/ceedling/vendor/unity/src/unity.h \
+ C:/REX/StepperMotor/vendor/ceedling/vendor/unity/src/unity_internals.h \
+ C:/REX/StepperMotor/Inc/MotorController.h
+
+C:/REX/StepperMotor/Inc/InfoVerifier.h:
+
+C:/REX/StepperMotor/Inc/MotorController.h:
+
+C:/REX/StepperMotor/vendor/ceedling/vendor/unity/src/unity.h:
+
+C:/REX/StepperMotor/vendor/ceedling/vendor/unity/src/unity_internals.h:
+
+C:/REX/StepperMotor/Inc/MotorController.h:
